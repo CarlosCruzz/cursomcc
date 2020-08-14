cb7495bb9645d4287a9453ed296b8a8b752b4226
Carlos Cruz <carloscruz1958@hotmail.com>
2020-08-13 21:48:41 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -70,6 +70,11 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
+		Categoria cat3 = new Categoria(null,"Cama mesa e Banho");
+		Categoria cat4 = new Categoria(null,"Eletrônicos");
+		Categoria cat5 = new Categoria(null,"Jardinagem");
+		Categoria cat6 = new Categoria(null,"Decoração");
+		Categoria cat7 = new Categoria(null,"Perfumaria");
 		
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null,"Impressora",800.00);
@@ -126,7 +131,7 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		cli1.getEndereco().addAll(Arrays.asList(e1,e2));
 		
-		catrepo.saveAll(Arrays.asList(cat1,cat2));
+		catrepo.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		prodrepo.saveAll(Arrays.asList(p1,p2,p3));
 		estrepo.saveAll(Arrays.asList(est1,est2));
 		cidrepo.saveAll(Arrays.asList(c1,c2,c3));
